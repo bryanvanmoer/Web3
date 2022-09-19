5e5ae4fe48dc5aefaf32eb8c05537e5239da5cd5
VAN MOER BRYAN <bryan.vanmoer@student.vinci.be>
2022-09-19 23:00:58 +0200

    1.1-1.2 : import image

--- exercises/Module1/1.1-1.2/src/components/Header/Header.jsx
+++ exercises/Module1/1.1-1.2/src/components/Header/Header.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import logoIPL from "./logo.png";
 
 const Header = (props) => {
   const course = props.course;
 
-  return <h1>{course}</h1>;
+  return (
+    <h1>
+      <img src={logoIPL} alt="" />
+      <br />
+      {course}
+    </h1>
+  );
 };
 
 export default Header;
