36d425a1f0cc86ac1d527e02ca6593cb9923cda5
VAN MOER BRYAN <bryan.vanmoer@student.vinci.be>
2022-09-19 22:57:20 +0200

    1.1-1.2 : CSS style

--- exercises/Module1/1.1-1.2/src/components/App/App.jsx
+++ exercises/Module1/1.1-1.2/src/components/App/App.jsx
@@ -3,7 +3,7 @@ import Header from "components/Header/Header";
 import Content from "components/Content/Content";
 import Total from "components/Total/Total";
 
-import "./style.css";
+import "./app.css";
 
 const App = () => {
   const course = "Half Stack application development";
