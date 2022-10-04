2b6271b8396d1122cd7b57149fc844e36579f53e
VAN MOER BRYAN <bryan.vanmoer@student.vinci.be>
2022-10-04 12:49:43 +0200

    module2 : localStorage

--- exercises/Module2/module2/src/components/App/App.jsx
+++ exercises/Module2/module2/src/components/App/App.jsx
@@ -4,16 +4,18 @@ import Display from "components/Display/Display";
 import Button from "components/Button/Button";
 
 const App = () => {
-  const [counter, setCounter] = useState(0);
+  const val = JSON.parse(localStorage.getItem("counter"));
+  const [counter, setCounter] = useState(val);
 
   const changeCount = (delta) => {
     const newCounter = counter + delta;
     setCounter(newCounter);
+    localStorage.setItem("counter", JSON.stringify(newCounter));
   };
 
   return (
     <div>
-      <Display counter={counter} />µ
+      <Display counter={counter} />
       <Button changeCount={changeCount} delta={1} text="plus" />
       <Button changeCount={changeCount} delta={-1} text="minus" />
       <Button changeCount={changeCount} delta={-counter} text="zero" />
